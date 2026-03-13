4bc3560a300454db4adb0de67a0ce641dd6ff0cb
DamianGrudzien <grudzien1694@gmail.com>
2026-03-13 19:44:24 +0100

    Add polishWords.js with a list of Polish words for password generation

---
+++ polishWords.js
@@ -0,0 +1,26 @@
+// List of Polish words for password generation
+const polishWords = [
+    "cześć",
+    "dziękuję",
+    "proszę",
+    "tak",
+    "nie",
+    "kot",
+    "pies",
+    "dom",
+    "szkoła",
+    "woda",
+    "drzewo",
+    "słońce",
+    "księżyc",
+    "gwiazda",
+    "motyl",
+    "serce",
+    "kwiat",
+    "miłość",
+    "przyjaciel",
+    "rodzina"
+];
+
+// Export the list
+module.exports = polishWords;
