2c8031fcb2db868d8f46a05ce8aa73c71d7be6c9
DamianGrudzien <grudzien1694@gmail.com>
2026-03-13 20:12:32 +0100

    Add script.js for Polish password generator functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+// Polish Password Generator Functionality
+
+function generatePassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+// Sample usage:
+console.log(generatePassword(12)); // Generates a password of length 12
+
+// Additional interactive functionalities can be added here.
